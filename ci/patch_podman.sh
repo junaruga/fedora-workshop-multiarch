1bfd20d9300126cdfe9d68ca6f96b0a7cc3531ad
Jun Aruga <junaruga@users.noreply.github.com>
2019-08-04 15:23:00 +0200

    Update .traivs.yml (#18)
    Change the order of test cases.
    Update comments.
    
    podman: debug.

--- ci/patch_podman.sh
+++ ci/patch_podman.sh
@@ -1,19 +1,40 @@
 #!/bin/bash
 set -xe
 
-mkdir -p ~/.config/containers
-cp -p ci/registries.conf ~/.config/containers/
-cp -p ci/storage.conf ~/.config/containers/
-USER_ID="$(id -u)"
+dpkg-query -L containers-common
+dpkg-query -L podman
+
+# USER_ID="$(id -u)"
 USER_NAME="$(id -un)"
-sed -i "s/@USER_ID@/${USER_ID}/" ~/.config/containers/storage.conf
-sed -i "s/@USER_NAME@/${USER_NAME}/" ~/.config/containers/storage.conf
+CWD="$(pwd)"
+
+# registries.conf
+dpkg -S /etc/containers/registries.conf
+cat /etc/containers/registries.conf
+sudo cp -p ci/registries.conf /etc/containers/registries.conf
+# mkdir -p ~/.config/containers
+# cp -p ci/registries.conf ~/.config/containers/
+
+# storage.conf
+dpkg -S /etc/containers/storage.conf
+cat /etc/containers/storage.conf
+
+sed -i "s|@CWD@|${CWD}|" ci/root_storage.conf.in
+sudo cp -p ci/root_storage.conf.in /etc/containers/storage.conf
+
+# cp -p ci/storage.conf ~/.config/containers/
+# sed -i "s/@USER_ID@/${USER_ID}/" ~/.config/containers/storage.conf
+# sed -i "s/@USER_NAME@/${USER_NAME}/" ~/.config/containers/storage.conf
 
 # No such file or directory
 var_dirs="
     /var/lib/containers
     /var/run/containers
     /var/run/libpod
+    /var/lib/containers/storage
+    /var/lib/containers/storage/overlay
+    ${CWD}/root/var/run/containers/storage
+    ${CWD}/root/var/lib/containers/storage
 "
 for var_dir in ${var_dirs}; do
     sudo ls "${var_dir}" || true
@@ -21,3 +42,4 @@ for var_dir in ${var_dirs}; do
     sudo chown -R "${USER_NAME}" "${var_dir}"
 done
 podman info --debug
+sudo podman info --debug
